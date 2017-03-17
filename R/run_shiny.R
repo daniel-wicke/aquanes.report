72b983917a309dc5352ee027d8fc12f8ec1aeb80
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-03-17 12:25:31 +0100

    rename app & include option to specify path for mySQL config file (".my.cnf")

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -1,22 +1,25 @@
 #'Runs Shiny app for an AQUANES site
 #'@param siteName site name for shiny app (default: "haridwar")
 #'@param use_live_data should live data be used (default: FALSE)
+#'@param mySQL_conf file path to mySQL config file (.my.cnf). Only used if
+#'parameter use_live_data is TRUE and there is no .my.cnf in the app folder for
+#'the selected site (default: NULL)
 #'@param launch.browser If true, the system's default web browser will be
 #'launched automatically after the app is started (default: TRUE)
 #'@param ... further arguments passed to shiny::runApp()
 #'@importFrom shiny runApp
 #'@export
-run_shinyapp <- function(siteName = "haridwar",
-                         use_live_data = FALSE,
-                         launch.browser = TRUE,
-                         ...) {
+run_app <- function(siteName = "haridwar",
+                    use_live_data = FALSE,
+                    mySQL_conf = NULL,
+                    launch.browser = TRUE,
+                    ...) {
 
 
   use_live_data <- toupper(use_live_data)
 
   shinyDir <- system.file("shiny", package = "aquanes.report")
   appDir <- file.path(shinyDir, siteName)
-
 
   if (!siteName %in% dir(shinyDir)) {
     msg <- sprintf("Could not find shiny app directory for %s.\n
@@ -25,6 +28,24 @@ run_shinyapp <- function(siteName = "haridwar",
                     paste(dir(shinyDir), collapse = ", "))
 
     stop(msg, call. = FALSE)
+  }
+
+  mySQL_conf_path <- file.path(appDir, ".my.cnf")
+
+  if (use_live_data) {
+
+    if (!is.null(mySQL_conf)) {
+      file.copy(from = mySQL_conf,
+                to = mySQL_conf_path)
+    }
+
+    if (!file.exists(mySQL_conf_path)) {
+      msg <- sprintf("No '.my.cnf' file located under: %s\n.
+           Please once specify the path to a valid MySQL config file with parameter
+              'mySQL_conf'", appDir)
+      stop(msg,
+           call. = FALSE)
+    }
   }
 
 
