e73c23e1df541d7f346988ac0472db6ccf92fd0c
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-03-20 16:04:49 +0100

    prepare time period aggregation (year, month, day, hour, minute) & new function import_data_haridwar() performing operational & analytics import

---
+++ R/group_datetime.R
@@ -0,0 +1,61 @@
+#'Group DateTime by user defined period (year, month, day, hour, minute)
+#' @param df a data frame as retrieved by import_data_haridwar()
+#' (default: system.file("shiny/haridwar/data/161101Monitoring_AquaNES_4014_Haridwar_KWB.xlsx",
+#' package = "aquanes.report"))
+#' @param by an aggregation time step of "year", "month", "day", "hour", "minute"
+#' (default: "day)
+#' @param fun function to be used for grouping measurement data of column ParameterValue
+#' (default: median)
+#' (default: system.file("shiny/haridwar/.my.cnf", package = "aquanes.report"))
+#' @param col_datetime column name of datetime column (default: DateTime)
+#' @param col_datatype column name of data type column (default: DateType)
+#' @return returns data frame with data aggregated according to user defined
+#' aggregation time step
+#' @import dplyr
+#' @export
+
+group_datetime <- function(df,
+                           by = "day",
+                           fun = "median",
+                           col_datetime = "DateTime",
+                           col_datatype = "DataType") {
+
+
+by <- tolower(by)
+
+grp_list <- list(year = "%Y-01-01 00:00:00",
+                 month = "%Y-%m-01 00:00:00",
+                 day = "%Y-%m-%d 00:00:00",
+                 hour = "%Y-%m-%d %H:00:00",
+                 minute = "%Y-%m-%d %H:%M:00")
+
+if (!by %in% names(grp_list)) {
+
+  msg <- sprintf("'%s' is no valid aggregation time step!\n Please select one of: %s for parameter 'by')",
+                 by,
+                 paste(names(grp_list), collapse = ", "))
+  stop(msg,
+       call. = FALSE)
+
+
+}
+
+datetime_org <- df[,col_datetime]
+tz_org <- base::check_tzones(datetime_org)
+df[,col_datetime] <- as.POSIXct(format(df[,col_datetime],
+                                        format = grp_list[[by]]),
+                                tz = tz_org)
+
+
+if (by == "day") by <- "dai"
+
+df[,col_datatype] <- sprintf("%s (%sly %s)", df[,col_datatype], by, fun)
+
+
+df <- df %>% dplyr::group_by_(.dots = dplyr::setdiff(names(df), "ParameterValue")) %>%
+             dplyr::summarise_("ParameterValue" = sprintf("stats::%s(ParameterValue)", fun)) %>%
+             as.data.frame()
+
+return(df)
+}
+
---
+++ R/import_data.R
@@ -0,0 +1,132 @@
+#'Imports Haridwar data
+#' @param analytics_path Define path of analytics EXCEL spreadsheet to be
+#' imported (default: system.file(file.path("shiny/haridwar/data",
+#' "161101Monitoring_AquaNES_4014_Haridwar_KWB.xlsx"),
+#' package = "aquanes.report"))
+#' @param operation_mySQL_conf column name pattern for identifying raw data
+#' (default: system.file("shiny/haridwar/.my.cnf", package = "aquanes.report"))
+#' @param operation_meta_path path to table with meta data for operational
+#' parameters (default: system.file(file.path("shiny/haridwar/data",
+#' "operation_parameters.csv"), package = "aquanes.report"))
+#' @param debug if TRUE print debug messages (default: TRUE)
+#' @return returns data frame with Haridwar raw data (operation & analytics)
+#' @import readxl
+#' @import tidyr
+#' @import dplyr
+#' @importFrom plyr rbind.fill
+#' @importFrom utils read.csv
+#' @export
+
+import_data_haridwar <- function(analytics_path = system.file(file.path("shiny",
+  "haridwar/data/161101Monitoring_AquaNES_4014_Haridwar_KWB.xlsx"),
+  package = "aquanes.report"),
+  operation_mySQL_conf = system.file("shiny/haridwar/.my.cnf",
+                                      package = "aquanes.report"),
+  operation_meta_path = system.file(file.path("shiny/haridwar/data",
+                        "operation_parameters.csv"),
+                        package = "aquanes.report"),
+                                 debug = TRUE) {
+
+
+  if (!file.exists(analytics_path)) {
+    msg <- sprintf("No analytics file %s is located under: %s",
+                   basename(analytics_path),
+                   dirname(analytics_path))
+    stop(msg,
+         call. = FALSE)
+
+  }
+
+
+  if (!file.exists(operation_mySQL_conf)) {
+    msg <- sprintf("No '.my.cnf' file located under: %s\n.
+                   Please once specify the path to a valid MySQL config file with parameter
+                   'mySQL_conf'", dirname(operation_mySQL_conf))
+    stop(msg,
+         call. = FALSE)
+  }
+
+###############################################################################
+###############################################################################
+###############################################################################
+#### Site 1: Haridwar
+###############################################################################
+###############################################################################
+###############################################################################
+
+###############################################################################
+#### 1) Import analytics data from EXCEL spreadsheet
+###############################################################################
+if(debug) print("### Step 1: Import analytics data ##########################")
+### Define directory (variable "xlsDir") and filename (variable "xlsName) of
+### EXCEl spreadsheet to be imported
+xlsPath <- analytics_path
+
+
+### Use case 2) Import a multiple sheets from a .xls file
+excludedSheets <- c("Parameters",
+                    "Location",
+                    "Sites",
+                    "Summary",
+                    "Observations",
+                    "dP_Manometer_2.Regelung",
+                    "Flow"
+)
+
+all_sheets <- readxl::excel_sheets(xlsPath)
+
+analytics_to_import <- all_sheets[!all_sheets %in% excludedSheets]
+
+
+
+analytics_4014 <- import_sheets(xlsPath = xlsPath,
+                                sheets_analytics = analytics_to_import)
+
+
+
+###############################################################################
+#### 2) Operational data
+###############################################################################
+
+if (debug) print("### Step 2: Import operational data ##########################")
+
+
+
+#### 2.1) Import
+
+operation <- import_operation(mysql_conf = operation_mySQL_conf)
+
+
+if (debug) print("### Step 3: Standardise analytics & operational data ##########################")
+
+drop.cols <- c("DateTime", "AnlagenID", "LocationName", "id", "localTime")
+
+operation_list <- tidyr::gather_(data =  operation,
+                                 key_col = "ParameterCode",
+                 value_col = "ParameterValue",
+                 gather_cols = dplyr::setdiff(names(operation),drop.cols))
+
+operation_para_names <- utils::read.csv(file = operation_meta_path,
+                                        stringsAsFactors = FALSE )
+
+
+operation_list <- operation_list %>%
+  dplyr::left_join(operation_para_names) %>%
+  dplyr::mutate_(Source = "as.character('online')")
+
+
+haridwar_raw_list <- plyr::rbind.fill(operation_list,
+                                      analytics_4014 %>%
+                                        dplyr::mutate_(Source = "as.character('offline')"))
+
+haridwar_raw_list$DataType <- "raw"
+
+drop.cols <- c("id", "AnlagenID", "Who", "Comments","localTime", "LocationName", "LocationID")
+
+haridwar_raw_list  <- haridwar_raw_list[,dplyr::setdiff(names(haridwar_raw_list),drop.cols)]  %>%
+  dplyr::filter_("!is.na(ParameterValue)")
+
+haridwar_raw_list$SiteName[is.na(haridwar_raw_list$SiteName)] <- "Online"
+
+return(haridwar_raw_list)
+}
