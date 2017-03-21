534f4266b908838b6b63e3ab8a8da49c9b7bc3a6
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-03-21 13:24:13 +0100

    adapt to enable app to work off- & online!

--- inst/shiny/haridwar/global.R
+++ inst/shiny/haridwar/global.R
@@ -2,7 +2,18 @@ use_live_data <- FALSE
 
 if (use_live_data) {
 
-haridwar_raw_list <- import_data_haridwar()
+analytics <- file.path(getwd(),
+                       "data/161101Monitoring_AquaNES_4014_Haridwar_KWB.xlsx")
+
+mySQL <- file.path(getwd(),
+                   ".my.cnf")
+
+op_meta <- file.path(getwd(),
+                     "data/operation_parameters.csv")
+
+haridwar_raw_list <- import_data_haridwar(analytics_path = analytics,
+                                          operation_mySQL_conf = mySQL,
+                                          operation_meta_path = op_meta)
 
 saveRDS(haridwar_raw_list, file = "data/haridwar_raw_list.Rds")
 } else {
