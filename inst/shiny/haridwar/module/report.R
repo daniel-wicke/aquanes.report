05aa56fc3c5055e2b03e19a6907d043a3c0ac5cd
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-03-22 14:24:22 +0100

    add Word & PDF download options for report

--- inst/shiny/haridwar/module/report.R
+++ inst/shiny/haridwar/module/report.R
@@ -43,7 +43,9 @@ ui_report <- function(...) {
                     choices = unique(haridwar_raw_list$ParameterName[haridwar_raw_list$Source == "offline"]),
                     multiple = TRUE,
                     selected = unique(haridwar_raw_list$ParameterName[haridwar_raw_list$Source == "offline"])[1]),
-        downloadButton("report_download", "Download report")#,
+        radioButtons("report_format", "Report format", c("HTML", "PDF", "Word"),
+                      inline = TRUE),
+        downloadButton("report_download", "Generate & download report")#,
         # selectInput("dataset", "Choose a dataset to download:",
         #             choices = c("data_plot1", "data_plot2")),
         # downloadButton('downloadData', 'Download data'),
@@ -144,13 +146,65 @@ server_report <- function(...) {
     includeHTML(create_report())
   )
 
-  output$report_download <- downloadHandler(filename = "automated_report.html",
-                                            content = function(file) {
-                                              file.copy(from = create_report(),
-                                                        to =  file)
-                                              })
+  # output$report_download <- downloadHandler(filename = "automated_report.html",
+  #                                           content = function(file) {
+  #                                             file.copy(from = create_report(),
+  #                                                       to =  file)
+  #                                             })
 
 
+  output$report_download <- downloadHandler(
+    filename = function() {
+      paste('automated_report', sep = '.', switch(
+        input$report_format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+      ))
+    },
+
+    content = function(file) {
 
 
+      if (input$report_format == "HTML") {
+        file.copy(from = create_report(), to =  file)
+      } else {
+
+      tdir <- tempdir()
+      tempReport <- file.path(tdir, "report.Rmd")
+      file.copy(from = "report/report.Rmd",
+                to = tempReport,
+                overwrite = TRUE)
+
+      # Set up parameters to pass to Rmd document
+      params <- list(run_as_standalone = FALSE,
+                     report_data = report_data_agg(),
+                     report_aggregation = input$report_aggregation,
+                     report_sitenames = input$report_sitenames,
+                     report_parameters_online = input$report_parameters_online,
+                     report_parameters_offline = input$report_parameters_offline,
+                     report_daterange = input$report_daterange,
+                     report_timezone = input$report_timezone)
+
+      # Knit the document, passing in the `params` list, and eval it in a
+      # child of the global environment (this isolates the code in the document
+      # from the code in this app).
+      ofile <-  file.path(tdir, paste('automated_report', sep = '.',
+                      switch(input$report_format,
+                             PDF = 'pdf',
+                             Word = 'docx')))
+
+
+      output_format <- switch(input$report_format,
+                              PDF = "pdf_document",
+                              Word = "word_document")
+
+      rmarkdown::render(input = tempReport,
+                        output_file = ofile,
+                        output_format = output_format,
+                        params = params,
+                        envir = new.env(parent = globalenv()))
+      file.copy(from = ofile,
+                  to = file)
+
+      }
+    }
+  )
 }
